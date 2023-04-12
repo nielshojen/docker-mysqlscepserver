65380024b094e65b7b03edd28f8a47988924f3ed
Niels Højen <niels@hojen.net>
2023-04-12 16:30:11 +0200

    update to use env vars

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # docker-mysqlscepserver
+
+Environment variables to configure the server
+
+ | Environment Variable | 
+ |--
+ | SCEP_API_KEY |
+ | SCEP_CA_PASS |
+ | SCEP_CHALLENGE_PASSWORD |
+ | SCEP_LOG_DEBUG |
+ | SCEP_DSN |
+ | SCEP_HTTP_LISTEN |
--- run.sh
+++ run.sh
@@ -5,41 +5,15 @@
 execServe="/usr/local/bin/mysqlscepserver"
 
 # API Key - Required
-if [[ ! ${API_KEY} ]]; then
+if [[ ! ${SCEP_API_KEY} ]]; then
   /bin/echo "No API Key Set - Exiting"
   exit 1
-else
-  execServe="${execServe} -api ${API_KEY}"
-fi
-
-# CA Password for ca.key
-if [[ ${CAPASS} ]]; then
-  execServe="${execServe} -capass ${CAPASS}"
-fi
-
-# Static challenge password (disables dynamic challenges)
-if [[ ${CHALLENGE} ]]; then
-  execServe="${execServe} -challenge ${CHALLENGE}"
-fi
-
-# Set debug
-if [[ "${DEBUG}" = "true" ]]; then
-  execServe="${execServe} -debug"
 fi
 
 # DSN - Required
-if [[ ! ${DBUSER} ]] && [[ ! ${DBPASS} ]] && [[ ! ${DBNAME} ]]; then
-  /bin/echo "Cannot set DSN - Exiting"
+if [[ ! ${SCEP_DSN} ]]; then
+  /bin/echo "No DSN Set - Exiting"
   exit 1
-else
-  execServe="${execServe} -dsn '${DBUSER}:${DBPASS}@tcp(${DBHOST:=127.0.0.1}:${DBPORT:=3306})/${DBNAME}'"
 fi
-
-# Port to listen on (default ":8080")
-if [[ ${LISTEN} ]]; then
-  execServe="${execServe} -listen ${LISTEN}"
-fi
-
-echo "Starting using: $execServe"
 
 eval $execServe
